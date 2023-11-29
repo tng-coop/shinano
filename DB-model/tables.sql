6616895d709d2072dd8c007acbcd611bb748154d
Kei Hibino <ex8k.hibino@gmail.com>
2023-11-29 21:39:54 +0900

    DB: auto increment primary key. open timestamp

--- DB-model/tables.sql
+++ DB-model/tables.sql
@@ -1,6 +1,6 @@
 
 CREATE TABLE user
-       ( id            BIGINT PRIMARY KEY
+       ( id            BIGINT AUTO_INCREMENT PRIMARY KEY
        , email         VARCHAR(255) NOT NULL UNIQUE
        , passwd_hash   VARCHAR(512) NOT NULL
        , note          TEXT NOT NULL
@@ -10,12 +10,13 @@ CREATE TABLE user
        );
 
 CREATE TABLE job_entry
-       ( id            BIGINT PRIMARY KEY
+       ( id            BIGINT AUTO_INCREMENT PRIMARY KEY
        , attribute     CHAR(1) NOT NULL -- 'L': listing, 'S': seeking
        , user          BIGINT NOT NULL
        , title         VARCHAR(512) NOT NULL
        , description   TEXT NOT NULL
        , created_at    TIMESTAMP NOT NULL
        , updated_at    TIMESTAMP NOT NULL
-       , expired_at    TIMESTAMP
+       , opened_at     TIMESTAMP
+       , closed_at     TIMESTAMP
        );
