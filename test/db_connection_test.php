6661dddac653dde408f92a12155713b48d3e067c
Kei Hibino <ex8k.hibino@gmail.com>
2023-11-30 22:56:40 +0900

    update connection_test

--- test/db_connection_test.php
+++ test/db_connection_test.php
@@ -1,8 +1,17 @@
 <?php
 
-// $data_source_name = "{$sqlclient}:host={$dbhost};dbname={$dbname};charset=UTF8";
+// $dsn = "{$sqlclient}:host={$dbhost};dbname={$dbname};charset=...";
 
-$dbconn_ro = new PDO("mariadb:dbname=shinano_dev;host=localhost", "sdev_ro", "Kis0Shinan0DevR0"); // todo: charset
-echo $dbconn_ro;
+$dsn = "mysql:host=localhost;dbname=shinano_dev";
 
-?>
+$dbconn_ro = new PDO($dsn, "sdev_ro", "Kis0Shinan0DevR0"); // todo: charset
+$stmt_ro = $dbconn_ro->query("SELECT 1");
+$row = $stmt_ro->fetch(PDO::FETCH_ASSOC);
+print_r($row);
+
+$dbconn_rw = new PDO($dsn, "sdev_rw", "Kis0Shinan0DevRW"); // todo: charset
+$stmt_rw = $dbconn_rw->query("SELECT 1");
+$row = $stmt_rw->fetch(PDO::FETCH_ASSOC);
+print_r($row);
+
+ ?>
