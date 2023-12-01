93792d29df29bfc37708debccfe789aaf8989e5f
i-makinori <maau3p@gmail.com>
2023-12-01 13:49:52 +0900

    Add. PHP PDO based MySQL connecting source

--- studyyard/index.php
+++ studyyard/index.php
@@ -4,32 +4,47 @@ declare(strict_types=1);
 
 include_once(__DIR__ . "/./ingredients/utilities.php");
 
+// prepare template
 
 $tpl = new \NAMES\TemplateAndConfigs();
 
 $tpl->page_title = "index php here";
 
-
-$dbname = "shinano_dev";
-$dbhost = "localhost";
-$sqlclient = "mysql";
-
-$data_source_name = "{$sqlclient}:host={$dbhost};dbname={$dbname};charset=UTF8";
-
-
-//$dbconn_ro = new PDO("{$sqlclient}:dbname={$dbname};host={$dbhost}", $sql_ro_user, $sql_ro_pass);
+// parepare and execute DB and SQL
 
 
 try {
+    //$dbconn_ro = new PDO("{$sqlclient}:dbname={$dbname};host={$dbhost}", $sql_ro_user, $sql_ro_pass);
     $dbconn_ro = new PDO($data_source_name, $sql_ro_user, $sql_ro_pass);
-    //echo "aaa";
-
 } catch (Exception $e){
     echo $e->getMessage();
 }
 
-
 $show_conn_ro = print_r($dbconn_ro, true);
+
+/*
+   // example sql
+   $sql1 = "SELECT U.name, J.title, J.description, J.created_at, J.updated_at, J.opened_at, J.closed_at
+   FROM shinano_dev.user as U INNER JOIN job_entry AS J
+   ON U.id = J.user
+   WHERE attribute = 'L' AND U.email = @email
+   ORDER BY opened_at IS NULL ASC, created_at ASC;";
+   
+ */
+
+$sql1 = "SELECT name,email FROM shinano_dev.user;";
+
+$stmt=$dbconn_ro->prepare($sql1);
+$stmt->execute();
+
+$sql1_db_text="";
+while($row=$stmt->fetch()){
+    $sql1_db_text .= "{$row["name"]} :       {$row["email"]} \n";
+}
+
+
+
+// make contents
 
 $debug_area = <<<DEBUGAREA
 <hr />
@@ -38,15 +53,17 @@ Debug Area's text:
 ro_user: {$sql_ro_user}, pass: {$sql_ro_pass}
 ro_user: {$sql_rw_user}, pass: {$sql_rw_pass}
 conn: {$show_conn_ro}
+result: {$sql1_db_text}
 </pre>
 "{$sqlclient}:dbname={$dbname};host={$dbhost}", $sql_ro_user, $sql_ro_pass
 <hr />
 DEBUGAREA;
 
 
-// $debug_area=""; //
+// $debug_area=""; // if $debug_area is not needed ,uncomment
 
-// Todo: SQL contents selecting and show them
+
+
 $tpl->content_actual = <<<CONTENTINDEX
 
 <a href=\"./list_jobs.php\"> jobs </a>
@@ -61,7 +78,7 @@ CONTENTINDEX;
 
 
 
-// hoge
+// apply and echos template
 
 $tpl->eval_template("template.html");
 
