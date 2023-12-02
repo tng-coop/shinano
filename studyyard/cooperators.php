06e0245e43e6d374a7ba3f664f98964ee4d35c38
i-makinori <maau3p@gmail.com>
2023-12-02 09:02:45 +0900

    studyyard: update the method to make html text.

--- studyyard/cooperators.php
+++ studyyard/cooperators.php
@@ -18,21 +18,27 @@ $stmt = $wconn_ro->askdb($sql1);
 // make content_actual of cooperators
 
 function html_text_of_cooperators($statement){
+    
     $tml_text = "";
-
     $tml_text = "<div class='cooperators'>";
     while($row = $statement->fetch()){
+        [$name, $note, $email, $created_at]
+        = [htmlspecialchars($row['name']),
+           htmlspecialchars($row['note']),
+           htmlspecialchars($row["email"]),
+           $row["created_at"]];    
         $tml_text .= 
             ("<div class='cooperator'>" .
-             "  <h3> {$row['name']} </h3>". 
-             "  <p> <pre> {$row['note']} </pre> </p> " .
-             "  email: <span> {$row["email"]} </span>" . 
-             "  , created: <span> {$row["created_at"]} </span>".
+             "  <h3> {$name} </h3>". 
+             "  <p> <pre> {$note} </pre> </p> " .
+             "  <p style='color:blue;'> links of look for match </p>" .
+             "  email: <span> {$email} </span>" . 
+             "  , created: <span> {$created_at} </span>".
              "</div>" .
              "<hr /> \n");
     }
-
     $tml_text .= "</div>";
+    
     return $tml_text;
 }
 
--- studyyard/lookformatch.php
+++ studyyard/lookformatch.php
@@ -12,46 +12,55 @@ $wconn_ro = new \UTILS\WPDO($data_source_name, $sql_ro_user, $sql_ro_pass);
 
 $sql1 = "SELECT attribute,user,title,description,created_at,opened_at,closed_at FROM shinano_dev.job_entry;";
 
+
 $stmt = $wconn_ro->askdb($sql1);
+
 
 
 // make content_actual of cooperators
 
-function html_text_of_cooperators($statement){
+function html_text_of_matches_list($statement){
+    
     $tml_text = "";
-
     $tml_text = "<div class='cooperators'>";
     while($row = $statement->fetch()){
-        $tml_text .= 
+        [$t_title, $t_attribute, $t_created_at, $t_opened_at, $t_closed_at, $t_description]
+        = [htmlspecialchars($row['title']),
+           $row['attribute'],
+           $row['created_at'],
+           $row['opened_at'],
+           $row['closed_at'],
+           htmlspecialchars($row['description'])];
+        $tml_text
+            .= 
             ("<div class='cooperator'>" .
-             "  <h3> {$row['title']} </h3>" .
-             "  S/L: <span> {$row['attribute']} </span> , " .
-             "  created: <span> {$row['created_at']} </span> , " .
-             "  opened: <span> {$row['opened_at']} </span> , " .
-             "  closed: <span> {$row['closed_at']} </span> ," .
-             "  <p> <pre> {$row['description']} </pre> </p>" .
+             "  <h3> {$t_title} </h3>" .
+             "  S/L: <span> {$t_attribute} </span> , " .
+             "  created: <span> {$t_created_at} </span> , " .
+             "  opened: <span> {$t_opened_at} </span> , " .
+             "  closed: <span> {$t_closed_at} </span> ," .
+             "  <p> {$t_description} </p>" .
              "</div>" .
              "<hr /> \n");
     }
-
     $tml_text .= "</div>";
+    
     return $tml_text;
 }
 
-
-$cooperators_tml = html_text_of_cooperators($stmt);
+$matches_list_tml = html_text_of_matches_list($stmt);
 
 
 // prepare template
 
 $tpl = new \UTILS\TemplateAndConfigs();
 
-$tpl->page_title = "index php here";
+$tpl->page_title = "look for match";
 
 
 $tpl->content_actual = <<<CONTENTINDEX
 
-{$cooperators_tml}
+{$matches_list_tml}
 
 CONTENTINDEX;
 
