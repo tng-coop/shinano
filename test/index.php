3e228a9edd090fadc9a9774dbf697ad7316783ac
i-makinori <maau3p@gmail.com>
2023-12-07 12:06:41 +0900

    Add. contains list as test/index.php

---
+++ test/index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title> index </title>
+    </head>
+    <body>
+        <h4> contains list of the directory.</h4>
+        <?php
+        $dir = __DIR__;
+        $files = scandir($dir);
+
+        print("Contains of this directory (${dir}) is below. <br />");
+        print("beware the treat of '.' and '..' . <br />");
+
+        print("<ul>");
+        array_map(fn($file) => print("<li><a href='./${file}'> ${file} </a></li>"),
+                  $files);
+        print("</ul>");
+        ?>
+    </body>
+</html>
+
