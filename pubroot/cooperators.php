2f12193622b0cdc9820b2d58375abc6f8ed311c1
i-makinori <maau3p@gmail.com>
2023-12-08 21:51:57 +0900

    Clean. change the structure of file.

--- pubroot/cooperators.php
+++ pubroot/cooperators.php
@@ -5,17 +5,26 @@ declare(strict_types=1);
 include_once(__DIR__ . "/../lib/common.php");
 
 
-// parepare and execute DB and SQL
+if(! $login->user()){
+    // if not logged in, Page to ask login.
+    $tpl = new TemplateAndConfigs();
+    $tpl->page_title = "Please login - Shinano";
+    $tpl->content_actual = "to show cooperators, Please login.";
+    $tpl->eval_template("template.html");
 
+    exit();
+}
+
+// if logged in, prepare cooperators page.
+
+// parepare and execute DB and SQL
 $wconn_ro = new WPDO($data_source_name, $sql_ro_user, $sql_ro_pass);
 $sql1 = "SELECT name,email,public_uid,note,created_at FROM user;";
 $stmt = $wconn_ro->askdb($sql1);
 
-
 // make actual content of cooperators
 
 function html_text_of_cooperators($statement){
-    
     $tml_text = "";
     $tml_text = "<div class='cooperators'>";
     while($row = $statement->fetch()){
@@ -42,33 +51,18 @@ function html_text_of_cooperators($statement){
 }
 
 
-
 // prepare template
 
 $tpl = new TemplateAndConfigs();
-
-
-if ($login->user()) {
-    // if logged in, show cooperators.
     
-    $cooperators_tml = html_text_of_cooperators($stmt);    
-    
-    $tpl->page_title = "cooperators - Shinano";
+$cooperators_tml = html_text_of_cooperators($stmt);    
 
-    $tpl->content_actual = "{$cooperators_tml}";
+$tpl->page_title = "cooperators - Shinano";
 
-    // apply and echos template
-    $tpl->eval_template("template.html");
+$tpl->content_actual = "{$cooperators_tml}";
 
-}else {
-    // if not logged in, ask login.
+// apply and echos template
+$tpl->eval_template("template.html");
 
-    $tpl->page_title = "Please login - Shinano";
-
-    $tpl->content_actual = "Please login";
-
-    $tpl->eval_template("template.html");
-
-}
 
 ?>
