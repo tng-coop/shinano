caba377ff5727573020c65407b0c237a9ea8b7eb
i-makinori <maau3p@gmail.com>
2023-12-09 16:23:01 +0900

    renew. index.php

---
+++ pubroot/index.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+include_once(__DIR__ . "/../lib/common.php");
+
+// 
+
+$session_cooperator = $login->user();
+
+
+// index page for unlogged in cooperator
+if(! $session_cooperator){
+    $content_index_unloggedin=<<<CONTENT
+<h1>Shinano is the something.</h1>
+
+<p>here is Shinano document.
+text or text_file is rendered.</p>
+
+<p>once register to cooperator and login,
+you can edit and search cooperator, seeks and matchings. ......</p>
+
+<p><u>some</u> <u>document</u> <u>links</u></p>
+
+
+<h3>account login or create</h3>
+account <a href="./account_login.php">login</a> <a href="./account_create.php">create</a>
+
+
+CONTENT;
+    /*
+       // not finished to write
+       You can access cooperator page and seeking page,
+       You can write and require new you project which is looking for another cooperators,
+       show you or your to 
+       project, cooperator to
+     */
+
+    RenderByTemplate("template.html", "Index - Shinano -",
+                     $content_index_unloggedin);
+
+    exit();
+}
+
+
+// index page for logged in cooperator
+if($session_cooperator){
+    $content_index_loggedin = <<<CONTENT
+<h1>Shinano.</h1>
+Hello! {$session_cooperator['name']}! <br />
+
+<ul>
+  <li><a href='./cmenu_account.php'>edit your account</a></li>
+  <li><a href='./cmenu_seeks.php'>edit your seeks</a></li>
+  <li><a href='./cmenu_seek.php'>edit(or new) your seek</a></li>
+</ul>
+
+CONTENT;
+
+    RenderByTemplate("template.html", "Index - Shinano -",
+                     $content_index_loggedin);
+
+    exit();
+}
+
+
+
+
+
+
+// render HTML by template
+
+
+
+?>
