93e4fd9d111635b0f8850764be3d767c10c69d66
Yasuaki Kudo <yasu@tng.coop>
2023-12-10 10:29:24 +0900

    Enhance Logging and Streamline Execution in setup-users.sh
    - Added informative echo statements for better tracking of the script's execution process.
    - Improved error handling with more descriptive messages for missing database user or password variables.
    - Refactored the execution of MySQL commands for increased readability and maintainability. This includes replacing the heredoc approach with a variable-based command execution, thereby making the script more structured.

--- DB-model/setup-users.sh
+++ DB-model/setup-users.sh
@@ -35,20 +35,20 @@ if [ "$admin_mysql" = "" ]; then
 fi
 
 # Read config.ini and ensure only 'export' commands are executed
+echo "setup-users: Reading configuration from config.ini..."
 eval "$(php export-database-config.php | grep '^export ')"
 
 # Validate imported user and password variables
 if [ -z "$readonly_user" ] || [ -z "$readwrite_user" ] || \
    [ -z "$readonly_password" ] || [ -z "$readwrite_password" ]; then
-    echo "Error: Database user or password variables not set."
+    echo "setup-users: Error: Database user or password variables not set."
     exit 1
 fi
 
-set -x
-# $admin_mysql < drop-dev-tables.sql
+echo "setup-users: Starting MySQL user setup..."
 
-# Execute MySQL commands using a heredoc
-$admin_mysql <<SQL
+# Commands to be executed
+sql_commands=$(cat <<SQL
 CREATE SCHEMA IF NOT EXISTS shinano_dev;
 
 CREATE USER IF NOT EXISTS '$readonly_user'@localhost;
@@ -61,3 +61,13 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON shinano_dev.* TO '$readwrite_user'@local
 
 FLUSH PRIVILEGES;
 SQL
+)
+
+# Execute MySQL commands
+echo "setup-users: Executing MySQL commands..."
+if echo "$sql_commands" | $admin_mysql; then
+    echo "setup-users: MySQL user setup completed successfully."
+else
+    echo "setup-users: Error occurred during MySQL operations."
+    exit 1
+fi
