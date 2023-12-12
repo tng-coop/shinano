222cf85814d1749d4e7a82b595d2c5ec259d1dd1
i-makinori <maau3p@gmail.com>
2023-12-12 11:08:18 +0900

    Add. specific match page

--- pubroot/match.php
+++ pubroot/match.php
@@ -13,6 +13,26 @@ if(! $login->user()){
 
 // if logged in request, prepare contents.
 
+
+// utils. there is same pattern at the cooperator.php .
+function int_string_p(string $integer_string_maybe){
+    return (is_numeric($integer_string_maybe) && is_int(intval($integer_string_maybe)));
+}
+
+// Note:
+// addressing method for specific job_entry 
+// is going to be changed from jobentry.id into user each own jobentry's id.
+
+// parse GET request
+
+if($request_method == "GET") {
+    if(isset($_GET['eid']) && int_string_p($_GET['eid'])){
+        $request_entry_id = intval($_GET['eid']);
+    } else {
+        $request_entry_id = false;
+    }
+}
+
 // ask DB
 
 $sql1
@@ -20,38 +40,37 @@ $sql1
     . "    J.id, J.attribute, J.user, J.title, J.description, J.created_at, J.opened_at, J.closed_at"
     . "  FROM user as U INNER JOIN job_entry as J"
     . "    ON U.id = J.user"
-    . "  ORDER BY J.id"
+    . "  WHERE J.id = :entry_id"
     . ";";
 
-$job_entries = db_ask_ro($sql1, [], \PDO::FETCH_ASSOC);
+$job_entry = db_ask_ro($sql1, ['entry_id' => $request_entry_id], \PDO::FETCH_ASSOC)[0];
 
 // make content_actual of cooperators
 
-function html_text_of_matches_list($job_entries){
-    $key_names = array_keys($job_entries[0]);
+function html_text_of_matches_list($job_entry){
+    $key_names = array_keys($job_entry);
 
     // content
-    $tml_text = "";
-    $tml_text = "<div class='cooperators'>";
-    foreach($job_entries as $row){
+    if(!(($job_entry)===[])){
+        $tml_text = "";
+        $tml_text = "<div class='job_entry'>";
 
         // set key_value
         $vals = []; // values
         foreach($key_names as $key){
-            $vals[$key] = h($row[$key]);
+            $vals[$key] = h($job_entry[$key]);
         }
 
         // content of row
+        //$description_omitted = mb_strimwidth($vals['description'], 0, 74*3, '...', 'UTF-8' );
         $listing_or_seeking = ($vals['attribute'] =='L'  ?  'Listing' :
                                ($vals['attribute']=='S' ?  'Seeking' : 'showing'));
-        $detail_url = "{$pubroot}match.php?eid={$vals['id']}";
-        $description_omitted = mb_strimwidth($vals['description'], 0, 74*3, '...', 'UTF-8' );  // limit to 74*3 characters.
 
         global $pubroot;
         $tml_text
             .= 
             ("<div class='look_for_seek'>" .
-             "  <a href='{$detail_url}'> <h3> {$vals['title']} </h3> </a>" .
+             "  <a href=''> <h3> {$vals['title']} </h3> </a>" .
              "  {$listing_or_seeking} by " .
              "  <a href='{$pubroot}cooperator.php?puid={$vals['public_uid']}'>{$vals['name']}</a> <br />" .
              "  id: <span> {$vals['id']} </span> , " .
@@ -59,17 +78,16 @@ function html_text_of_matches_list($job_entries){
              "  created: <span> {$vals['created_at']} </span> , " .
              "  opened: <span> {$vals['opened_at']} </span> , " .
              "  closed: <span> {$vals['closed_at']} </span> ," .
-             "  <p><pre style='display: inline;'> {$description_omitted}</pre>" .
-             "     <a href='{$detail_url}'>(detail)</a></p>" .
+             "  <p><pre style='display: inline;'> {$vals['description']}</pre>" .
              "</div>" .
              "<hr /> \n");
+        $tml_text .= "</div>";
     }
-    $tml_text .= "</div>";
-    
+
     return $tml_text;
 }
 
-$matches_list_tml = html_text_of_matches_list($job_entries);
+$matches_list_tml = html_text_of_matches_list($job_entry);
 
 
 // prepare and render by template
