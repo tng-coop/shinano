c31548105bba911108f64d74bbda2b5a4be9d21e
Kei Hibino <ex8k.hibino@gmail.com>
2023-12-18 21:27:17 +0900

    local-things: table

--- DB-model/tables.sql
+++ DB-model/tables.sql
@@ -10,6 +10,7 @@ CREATE TABLE user
        , email         VARCHAR(255) NOT NULL UNIQUE
        , passwd_hash   VARCHAR(512) NOT NULL
        , public_uid    BIGINT NOT NULL UNIQUE
+       , last_thing    BIGINT NOT NULL
        , name          VARCHAR(255) NOT NULL
        , note          TEXT NOT NULL
        , created_at    TIMESTAMP NOT NULL
@@ -18,31 +19,34 @@ CREATE TABLE user
        );
 
 CREATE TABLE job_entry
-       ( id            BIGINT AUTO_INCREMENT PRIMARY KEY
+       ( user          BIGINT NOT NULL
+       , id_on_user    BIGINT NOT NULL
        , attribute     CHAR(1) NOT NULL -- 'L': listing, 'S': seeking
-       , user          BIGINT NOT NULL
        , title         VARCHAR(512) NOT NULL
        , description   TEXT NOT NULL
        , created_at    TIMESTAMP NOT NULL
        , updated_at    TIMESTAMP NOT NULL
        , opened_at     TIMESTAMP
        , closed_at     TIMESTAMP
+
+       , PRIMARY KEY (user, id_on_user)
        );
 
+
 CREATE UNIQUE INDEX job_entry_user_opened_created
-       ON job_entry (user, opened_at, created_at, id);
+       ON job_entry (user, opened_at, created_at, id_on_user);
 
 CREATE UNIQUE INDEX job_entry_attribute_opened_created_user
-       ON job_entry (attribute, opened_at, created_at, user, id);
+       ON job_entry (attribute, opened_at, created_at, user, id_on_user);
 
 
 -- pre_user used for email authorization when create account.
 -- which saves tokens (needed to be temporary).
-CREATE TABLE pre_user  
+CREATE TABLE pre_user
        ( id            BIGINT AUTO_INCREMENT PRIMARY KEY
        , urltoken      VARCHAR(255) NOT NULL
        , email         VARCHAR(255) NOT NULL
        , date          TIMESTAMP NOT NULL
-       , flag          TINYINT(1) NOT NULL DEFAULT 0 
+       , flag          TINYINT(1) NOT NULL DEFAULT 0
        -- flag ... email'd user is ... 0: not_registerd, 1:registerd.
        );
