c28ad013de2773ac49e98cf3bbf03ab7ce79cfc5
i-makinori <maau3p@gmail.com>
2023-12-19 11:30:19 +0900

    Clean. sepalate commonly generating function for tml into tml_templates.php.

--- lib/common.php
+++ lib/common.php
@@ -6,6 +6,7 @@ include_once(__DIR__ . '/./utilities.php');
 include_once(__DIR__ . '/./transactions.php');
 include_once(__DIR__ . '/./csrf.php');
 include_once(__DIR__ . '/./user_login.php');
+include_once(__DIR__ . '/./tml_templates.php');
 
 // session start if not started.
 if (session_status() === PHP_SESSION_NONE) {
