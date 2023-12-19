8da3fe847cbacd1adb80c6edcf768f19d7b1c088
i-makinori <maau3p@gmail.com>
2023-12-19 20:24:27 +0900

    Change. mockdata inserting method.

--- DB-model/mockdata-inserts.sql
+++ DB-model/mockdata-inserts.sql
@@ -1,5 +1,8 @@
 -- INSERT Dummy datas into aleady exist table of shinano_*.
 
+-- currentry, move to DB-mode/mockdata/insert_mockdata.php with
+-- $ php insert_mockdata.php
+
 /*
 -- INSERT mocks of user
 LOAD DATA LOCAL INFILE './mockdata/user.csv'
@@ -10,8 +13,8 @@ LOAD DATA LOCAL INFILE './mockdata/user.csv'
      (@dummy, name, email, passwd_hash, note, created_at, updated_at);
 */
 
-
--- INSERT mocks of job listing
+/*
+-- INSERT mocks of job_entry
 LOAD DATA LOCAL INFILE './mockdata/job_entry.csv'
      INTO TABLE job_entry
      FIELDS TERMINATED BY ',' ENCLOSED BY '"'
@@ -26,3 +29,5 @@ UPDATE user SET last_thing =  1 WHERE id =  7;
 UPDATE user SET last_thing =  3 WHERE id =  4;
 UPDATE user SET last_thing =  1 WHERE id = 11;
 UPDATE user SET last_thing = 11 WHERE id =  2;
+*/
+
