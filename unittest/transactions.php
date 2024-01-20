6816762d9467f670977ce6eba5a4f522ce67e3de
Kei Hibino <ex8k.hibino@gmail.com>
2024-01-20 22:39:40 +0900

    postgres: simplify transaction unixtests. add informative tags for failed case

--- unittest/transactions.php
+++ unittest/transactions.php
@@ -13,15 +13,15 @@ if ($config === false) {
 
 $dsn = $config['database']['dsn'];
 
-function check_record1(PDO $conn, string $sql, $pred) {
+function check_record1(PDO $conn, string $tag, string $sql, $pred) {
     $stmt = $conn->query($sql);
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
     if ($pred($row)) {
-        echo "OK\n";
+        echo "OK {$tag}\n";
     } else {
         $show_row = print_r($row, true);
-        echo "failed: {$sql} \n  row : {$show_row} is not satisfied predicate !\n";
+        echo "failed: {$tag} : {$sql} \n  row : {$show_row} is not satisfied predicate !\n";
     }
 }
 
@@ -37,40 +37,33 @@ function show_records($stmt) {
 }
 
 function public_uid_from_email(PDO $conn, string $email) : int {
-    $stmt = $conn->prepare("SELECT public_uid FROM user WHERE email = ?");
+    $stmt = $conn->prepare('SELECT public_uid FROM "user" WHERE email = ?');
     $stmt->execute(array($email));
     return ($stmt->fetch(PDO::FETCH_NUM))[0];
 }
 
 \Tx\with_connection($dsn, $config['database']['readwrite_user'], $config['database']['readwrite_password'])(
     function($conn_rw) {
-        $emails_user_id=\TxSnn\user_id_lock_by_email($conn_rw, 'yamada@example.com');
-        if(!$emails_user_id){
-            echo "Mr Taro Yamada is not exist. \nThen, I make new user.\n";
-            \TxSnn\add_user($conn_rw, 'Taro Yamada', 'yamada@example.com', 'abcd', 'taro\'s note');
-            check_record1($conn_rw, 'SELECT * FROM user',
+        \TxSnn\add_user($conn_rw, 'Taro Yamada', 'yamada@example.com', 'abcd', 'taro\'s note');
+        check_record1($conn_rw, "add_user", 'SELECT * FROM "user"',
                       fn($row) => $row['email'] == 'yamada@example.com');
-        }else{
-            $conn_rw->rollBack();
-            echo "Mr Taro Yamada is already exist. \n";
-        }
         \TxSnn\add_job_listing($conn_rw, 'yamada@example.com', 'taro job listing', 'taro job description ...');
-        check_record1($conn_rw, "SELECT * FROM job_entry WHERE attribute = 'L'",
+        check_record1($conn_rw, "add_listing", "SELECT * FROM job_entry WHERE attribute = 'L'",
                       fn($row) => preg_match('/^taro job l/', $row['title']) && is_null($row['opened_at']));
         \TxSnn\add_job_seeking($conn_rw, 'yamada@example.com', 'taro job seeking', 'taro seeking job about ...');
-        check_record1($conn_rw, "SELECT * FROM job_entry WHERE attribute = 'S'",
+        check_record1($conn_rw, "add_seeking", "SELECT * FROM job_entry WHERE attribute = 'S'",
                       fn($row) => preg_match('/^taro job s/', $row['title']) && is_null($row['opened_at']));
         \TxSnn\open_job_listing($conn_rw, 'yamada@example.com', 1);
-        check_record1($conn_rw, "SELECT * FROM job_entry WHERE attribute = 'L'",
+        check_record1($conn_rw, "open_listing", "SELECT * FROM job_entry WHERE attribute = 'L'",
                       fn($row) => !is_null($row['opened_at']) && is_null($row['closed_at']));
         \TxSnn\open_job_seeking($conn_rw, 'yamada@example.com', 2);
-        check_record1($conn_rw, "SELECT * FROM job_entry WHERE attribute = 'S'",
+        check_record1($conn_rw, "open_seeking", "SELECT * FROM job_entry WHERE attribute = 'S'",
                       fn($row) => !is_null($row['opened_at']) && is_null($row['closed_at']));
         \TxSnn\close_job_listing($conn_rw, 'yamada@example.com', 1);
-        check_record1($conn_rw, "SELECT * FROM job_entry WHERE attribute = 'L'",
+        check_record1($conn_rw, "clsoe_listing", "SELECT * FROM job_entry WHERE attribute = 'L'",
                       fn($row) => !is_null($row['closed_at']));
         \TxSnn\close_job_seeking($conn_rw, 'yamada@example.com', 2);
-        check_record1($conn_rw, "SELECT * FROM job_entry WHERE attribute = 'S'",
+        check_record1($conn_rw, "clsoe_seeking", "SELECT * FROM job_entry WHERE attribute = 'S'",
                       fn($row) => !is_null($row['closed_at']));
     }
 );
